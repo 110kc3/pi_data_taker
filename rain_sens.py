6efca2b9556388e7d297f91cf7488b91b3508460
K C <110kc3@gmail.com>
2020-10-04 16:54:14 +0200

    SDS011 library + test code

--- rain_sens.py
+++ rain_sens.py
@@ -2,16 +2,16 @@
 # HIGH = no rain, LOW = rain detected
 
 from time import sleep
-from gpiozero import Buzzer, InputDevice
+from gpiozero import InputDevice
  
 
-no_rain = InputDevice(22)
+rain = InputDevice(22)
  
 
  
 while True:
-    if not no_rain.is_active:
+    if not rain.is_active:
         print("It's raining - get the washing in!")
-        # insert your other code or functions here
-        # e.g. tweet, SMS, email, take a photo etc.
+    else:
+	    print("Not raining")
     sleep(1)

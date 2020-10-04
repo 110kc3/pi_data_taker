f9e8bc676361b9a07827ca197f10c09f3896bfab
K C <110kc3@gmail.com>
2020-10-04 12:58:36 +0200

    Attaching DHT11 sensor

---
+++ dht11.py
@@ -0,0 +1,17 @@
+#import RPi.GPIO as GPIO
+import Adafruit_DHT
+import time
+
+# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+DHTSensor = Adafruit_DHT.DHT11
+
+# The pin which is connected with the sensor will be declared here
+GPIO_Pin = 27 #look at output of "python3 pinout" command
+
+while True:
+    humidity, temperature = Adafruit_DHT.read(DHTSensor, GPIO_Pin)
+    if humidity is not None and temperature is not None:
+       print("Temperature={0:0.1f}C  Humidity={1:0.1f}%".format(temperature, humidity))
+    else:
+       print("Sensor failure...");
+    time.sleep(3);
---
+++ setup.py
@@ -0,0 +1,6 @@
+# need python3 + pip3
+#sudo apt-get install python3 -y
+#sudo apt-get install python3-pip -y
+
+#need dht11 library for taking temp+humid data
+#sudo pip3 install Adafruit_DHT
